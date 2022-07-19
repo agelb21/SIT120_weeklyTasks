753fbacddee4da108d734a240db68991cfe3387c
agelb21 <agelbhauer@gmail.com>
2022-07-19 23:57:15 +1000

    vue install and todo list

---
+++ todo.js
@@ -0,0 +1,12 @@
+let vue = new Vue({
+    el: '#app',
+    data: {
+        todos:[
+            { title: 'task1', checked: true},
+            { title: 'task2', checked: true},
+            { title: 'task3', checked: true},
+            { title: 'task4', checked: false},
+        ],
+    },
+
+});
