4eeeadd2c34c21cb2cd44c52044ed8aa5e15a2be
agelb21 <87936603+agelb21@users.noreply.github.com>
2022-08-16 16:58:57 +1000

    w4 tasks

---
+++ Week4/index.js
@@ -0,0 +1,21 @@
+//task1 hello world
+
+var app = new Vue({
+    el: '#app',
+    data: {
+       // message: 'hello world!'
+    }
+});
+
+//task 2 v-if / v-else / v-show
+var app1 = new Vue({
+    el: '#food',
+    data: {
+        name: 'Satay Chicken',
+        nuts: true,
+        ingredients: ["Lime Juice", "Honey", "Soy Sauce", "Peanut Butter", "Coconut Milk", "Chicken Thigh"]
+    }
+})
+
+
+
